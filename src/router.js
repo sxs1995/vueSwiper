0f98daf6861838495b1039ddcf6c363f36894c74
shenxsh <shenxsh@inspurworld.com>
2019-05-31 14:44:56 +0800

    修改打包配置

--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+/*
+ * @Author: shenxsh
+ * @Date: 2019-05-31 10:21:03
+ * @LastEditTime: 2019-05-31 14:44:05
+ * @LastEditors: Do not edit
+ * @Description: 描述
+ */
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  base: "/dist/",
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
