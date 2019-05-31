1cf5073bde698cc6269c8becf68a89014b09faab
shenxsh <shenxsh@inspurworld.com>
2019-05-31 14:40:40 +0800

    打包出demo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
+/*
+ * @Author: shenxsh
+ * @Date: 2019-05-31 10:21:03
+ * @LastEditTime: 2019-05-31 10:52:45
+ * @LastEditors: Do not edit
+ * @Description: 描述
+ */
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+Vue.config.productionTip = false;
+Vue.use(VueAwesomeSwiper);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
+<!--
+ * @Author: shenxsh
+ * @Date: 2019-05-31 10:21:03
+ * @LastEditTime: 2019-05-31 11:44:06
+ * @LastEditors: Do not edit
+ * @Description: 常用的swiper移动端效果
+ -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>1.中间显示完整,两边少量显示</h3>
+    <swiper :options="option1">
+      <swiper-slide class="benefitBox" v-for="(item, index) in 8" :key="index">
+        <div class="box">item{{index}}</div>
+      </swiper-slide>
+    </swiper>
+    <h3>2.左侧显示完整,右侧少量显示</h3>
+    <swiper :options="option2">
+      <swiper-slide class="benefitBox" v-for="(item, index) in 8" :key="index">
+        <div class="box">item{{index}}</div>
+      </swiper-slide>
+    </swiper>
+    <h3>3.中间突出显示,两边缩放</h3>
+    <div class="option3">
+      <swiper :options="option3">
+        <swiper-slide class="benefitBox" v-for="(item, index) in 8" :key="index">
+          <div class="box">item{{index}}</div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    let _this = this;
+    return {
+      option1: {
+        slidesPerView: 1.18, //显示slide的数量
+        centeredSlides: true, //居中显示
+        loop: true, //循环
+        initialSlide: 1, //初始索引
+        spaceBetween: 15, //间距
+        on: {
+          click: function() {
+            // 获取点击的索引.
+            const realIndex = this.realIndex;
+            // 此处可自定义点击事件,注意此处this指向swiper,定义_this
+          }
+        }
+      },
+      option2: {
+        slidesPerView: 2.15,
+        spaceBetween: 15
+        // slidesOffsetBefore: -1 //左偏移
+      },
+      option3: {
+        slidesPerView: 1.49,
+        centeredSlides: true,
+        loop: true,
+        touchRatio: 1, //触摸距离与slide滑动距离的比率。
+        spaceBetween: 0
+      }
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+.home {
+  width: 100%;
+  padding-bottom: 40px;
+  .box {
+    background: red;
+    height: 200px;
+    color: #fff;
+    line-height: 200px;
+    text-align: center;
+    font-size: 24px;
   }
 }
-</script>
+.option3 {
+  .box {
+    height: 300px;
+  }
+  .swiper-slide:not(.swiper-slide-active) {
+    transition: 400ms;
+    transform: scale(0.85);
+  }
+}
+</style>
