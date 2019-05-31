8092e2cc60986a56960be7d42e169d9ba6431a55
shenxsh <shenxsh@inspurworld.com>
2019-05-31 14:45:30 +0800

    打包配置

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: shenxsh
+ * @Date: 2019-05-31 14:43:45
+ * @LastEditTime: 2019-05-31 14:44:01
+ * @LastEditors: Do not edit
+ * @Description: 描述
+ */
+module.exports = {
+  publicPath: "/dist/",
+  //webpack配置性能提示
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  }
+};
